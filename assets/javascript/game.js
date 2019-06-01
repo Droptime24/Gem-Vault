8095c22848c9ccd1a5045692ea3d6b076ef2ea2b
David Luce <david.luce@outlook.com>
2019-06-01 14:22:35 -0600

    setting up jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Target score
+$('#target').html(genNum(19,120));
+
+// Times lost 
+$('#losses').html(0);
+
+// Times won
+$('#wins').html(0);
+
+// Score
+$('#score').html(0);
+
+// Gems
+$('#gem1').html(genNum(1,12));
+$('#gem2').html(genNum(1,12));
+$('#gem3').html(genNum(1,12));
+$('#gem4').html(genNum(1,12));
+
+
+
+
+function genNum(min, max){
+    return Math.floor(Math.random() * (max - min) + (min))
+}
+
+
+    $('#gem1').on('click', function(){
+        var score = parseInt($('#score').html());
+        score += parseInt($(this).html());
+        $('#score').html(score);
+    });
