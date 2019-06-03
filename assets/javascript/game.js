4fb20c54d8fb27556f8a88b67e17344d6acc5867
David Luce <david.luce@outlook.com>
2019-06-02 21:04:17 -0600

    Updates to html outline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
-// Target score
-$('#target').html(genNum(19,120));
 
+var targetScore = 'target';
+var youLost = 'losses';
+var youWin = 'wins';
+var myScore = 'score';
+
+
+// Target score
+$('#target').html(genNum(19, 120));
 // Times lost 
 $('#losses').html(0);
 
@@ -11,35 +17,46 @@ $('#wins').html(0);
 $('#score').html(0);
 
 // Gems
-$('#gem1').html(genNum(1,12));
-$('#gem2').html(genNum(1,12));
-$('#gem3').html(genNum(1,12));
-$('#gem4').html(genNum(1,12));
+$('#gem1').html(genNum(1, 12));
+$('#gem2').html(genNum(1, 12));
+$('#gem3').html(genNum(1, 12));
+$('#gem4').html(genNum(1, 12));
 
+// Assign gem random numbers
+$('#gem1').on('click', function () {
+    var score = parseInt($('#score').html());
+    score += parseInt($(this).html());
+    $('#score').html(score);
+    if( score === target)youWin();
+});
 
+$('#gem2').on('click', function () {
+    var score = parseInt($('#score').html());
+    score += parseInt($(this).html());
+    $('#score').html(score);
 
+});
 
-function genNum(min, max){
-    return Math.floor(Math.random() * (max - min) + (min))
+$('#gem3').on('click', function () {
+    var score = parseInt($('#score').html());
+    score += parseInt($(this).html());
+    $('#score').html(score);
+});
+
+$('#gem4').on('click', function () {
+    var score = parseInt($('#score').html());
+    score += parseInt($(this).html());
+    $('#score').html(score);
+});
+
+// Global random number function
+function genNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + (min));
 }
 
-    $('#gem1').on('click', function(){
-        var score = parseInt($('#score').html());
-        score += parseInt($(this).html());
-        $('#score').html(score);
-    });
-    $('#gem2').on('click', function(){
-        var score = parseInt($('#score').html());
-        score += parseInt($(this).html());
-        $('#score').html(score);
-    });
-    $('#gem3').on('click', function(){
-        var score = parseInt($('#score').html());
-        score += parseInt($(this).html());
-        $('#score').html(score);
-    });
-    $('#gem4').on('click', function(){
-        var score = parseInt($('#score').html());
-        score += parseInt($(this).html());
-        $('#score').html(score);
-    });
+function youWin(wins, losses) {
+    var score = parseInt($('#score').html());
+    score += parseInt($(this).html());
+    
+};
+
